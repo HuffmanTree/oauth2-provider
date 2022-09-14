b205227fc8fb584a1eb44034e2f73e390c022eb4
François Guérin <francoisguerin1@hotmail.fr>
2022-09-14 22:59:17 +0200

    Add database client

---
+++ src/models/index.ts
@@ -0,0 +1,190 @@
+import { Sequelize } from "sequelize";
+import { Logger } from "../logger";
+import { UserModel } from "./user.model";
+
+/**
+ * @description Namespace associated with {@link OAuth2DatabaseClient}
+ */
+export namespace OAuth2DatabaseClient {
+  /**
+   * @description Configuration to partially pass to the constructor
+   */
+  export interface Config {
+    /**
+     * @description Name of the database targetted by the client
+     *
+     * @example "oauth2"
+     */
+    database: string;
+    /**
+     * @description Hostname of the database targetted by the client
+     *
+     * @example "localhost"
+     */
+    host: string;
+    /**
+     * @description Password to use during connection
+     *
+     * @example "secret"
+     */
+    password: string;
+    /**
+     * @description Port of the database targetted by the client
+     *
+     * @example 5432
+     */
+    port: number;
+    /**
+     * @description Username to use during connection
+     *
+     * @example "postgres"
+     */
+    username: string;
+  }
+}
+
+/**
+ * @description Client wrapping multiple database models
+ *
+ * @example
+ * const config = {
+ *   database: "oauth2",
+ *   username: "postgres",
+ *   host: "localhost",
+ *   port: 5432
+ * };
+ * const client = new OAuth2DatabaseClient(config);
+ *
+ * await client.connect();
+ *
+ * const users = await client.user.findAll();
+ * console.log("Number of users", users.length);
+ *
+ * await client.disconnect();
+ */
+export class OAuth2DatabaseClient {
+  toto?: number;
+
+  /**
+   * @description Name of the database targetted by the client
+   *
+   * @example "oauth2"
+   */
+  private _database: string;
+
+  /**
+   * @description Hostname of the database targetted by the client
+   *
+   * @example "localhost"
+   */
+  private _host: string;
+
+  private _logger: Logger;
+
+  /**
+   * @description Port of the database targetted by the client
+   *
+   * @example 5432
+   */
+  private _port: number;
+
+  private _sequelize: Sequelize;
+
+  /**
+   * @description User connected by the client
+   *
+   * @example "postgres"
+   */
+  private _username: string;
+
+  /**
+   * @description Build an instance of `OAuth2DatabaseClient`
+   *
+   * @param {OAuth2DatabaseClient.Config} config Configuration to use for the client
+   */
+  constructor({
+    database,
+    host,
+    password,
+    port,
+    username,
+  }: Partial<OAuth2DatabaseClient.Config>) {
+    this._database = database || process.env.SEQUELIZE_DATABASE || "postgres";
+
+    this._host = host || process.env.SEQUELIZE_HOST || "localhost";
+
+    this._port =
+      port ||
+      (process.env.SEQUELIZE_PORT && parseInt(process.env.SEQUELIZE_PORT)) ||
+      5432;
+
+    this._username = username || process.env.SEQUELIZE_USERNAME || "postgres";
+
+    this._logger = new Logger({ service: "OAuth2DatabaseClient" });
+
+    this._sequelize = new Sequelize({
+      database: this._database,
+      dialect: "postgres",
+      host: this._host,
+      password: password || process.env.SEQUELIZE_PASSWORD,
+      port: this._port,
+      username: this._username,
+      logging: this._logger.trace.bind(this._logger, {
+        database: this._database,
+        host: this._host,
+        port: this._port,
+        username: this._username,
+      }),
+    });
+
+    UserModel.initialize(this._sequelize);
+
+    this._logger.info({}, "Initialized models");
+  }
+
+  /**
+   * @description Connect the client to the database
+   *
+   * @example
+   * client
+   *  .connect()
+   *  .then(() => console.log("Client connected"))
+   *  .catch((err: Error) => console.error("Error while connecting to the database:" + err.message));
+   */
+  async connect(): Promise<void> {
+    await this._sequelize.sync({
+      alter: process.env.NODE_ENV === "development",
+    });
+
+    const ctx = {
+      database: this._database,
+      host: this._host,
+      port: this._port,
+      username: this._username,
+    };
+
+    this._logger.info(ctx, "Client is connected to database");
+  }
+
+  /**
+   * @description Disconnect the client from the database
+   *
+   * @example
+   * client
+   *  .disconnect()
+   *  .then(() => console.log("Client disconnected"))
+   *  .catch((err: Error) => console.error("Error while disconnecting from the database:" + err.message));
+   */
+  async disconnect(): Promise<void> {
+    await this._sequelize.close();
+
+    this._logger.info({}, "Client is closing");
+  }
+
+  /**
+   * @description Associated `UserModel` static instance
+   */
+  get user(): typeof UserModel {
+    return UserModel;
+  }
+}
---
+++ test/models/index.test.ts
@@ -0,0 +1,45 @@
+import { expect } from "chai";
+import { Sequelize } from "sequelize";
+import sinon from "sinon";
+import { OAuth2DatabaseClient } from "../../src/models/index";
+import { UserModel } from "../../src/models/user.model";
+
+let service: OAuth2DatabaseClient;
+
+describe("OAuth2DatabaseClient", () => {
+  before(() => {
+    const spy = sinon.spy(UserModel, "initialize");
+
+    service = new OAuth2DatabaseClient({});
+
+    expect(spy.calledOnce).to.be.true;
+
+    spy.restore();
+  });
+
+  it("connects the client", async () => {
+    const mock = sinon.mock(Sequelize.prototype);
+
+    mock.expects("sync").once().withArgs({ alter: false });
+
+    await service.connect();
+
+    mock.verify();
+    mock.restore();
+  });
+
+  it("disconnects the client", async () => {
+    const mock = sinon.mock(Sequelize.prototype);
+
+    mock.expects("close").once().withArgs();
+
+    await service.disconnect();
+
+    mock.verify();
+    mock.restore();
+  });
+
+  it("gets the associated user model", () => {
+    expect(service.user).to.equal(UserModel);
+  });
+});
