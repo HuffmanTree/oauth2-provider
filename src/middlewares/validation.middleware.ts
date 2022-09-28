a550a0506e1ca6eba18d9af20158bfeff812838d
François Guérin <francoisguerin1@hotmail.fr>
2022-09-28 23:06:10 +0200

    Add validation middleware

---
+++ src/middlewares/validation.middleware.ts
@@ -0,0 +1,51 @@
+import { ValidationService } from "../services/validation.service";
+import { Logger } from "../logger";
+import { JSONSchemaType } from "ajv";
+import { NextFunction, Request, Response } from "express";
+
+export class ValidationMiddleware {
+  private _service: ValidationService;
+
+  private _logger: Logger;
+
+  constructor(service: ValidationService) {
+    this._service = service;
+
+    this._logger = new Logger({ service: "ValidationMiddleware" });
+  }
+
+  validateRequest<BodyType, QueryType, ParamsType>({
+    bodySchema,
+    querySchema,
+    paramsSchema,
+  }: Partial<{
+    bodySchema: JSONSchemaType<BodyType>;
+    querySchema: JSONSchemaType<QueryType>;
+    paramsSchema: JSONSchemaType<ParamsType>;
+  }>) {
+    const logger = this._logger;
+    const validator = this._service.validate.bind(this._service);
+
+    async function callback(
+      req: Request<ParamsType, unknown, BodyType, QueryType>,
+      _res: Response,
+      next: NextFunction
+    ): Promise<void> {
+      const p: Array<Promise<BodyType | QueryType | ParamsType>> = [];
+
+      if (bodySchema) p.push(validator(req.body, bodySchema));
+      if (querySchema) p.push(validator(req.query, querySchema));
+      if (paramsSchema) p.push(validator(req.params, paramsSchema));
+
+      try {
+        await Promise.all(p);
+      } catch (err) {
+        logger.warn({}, "Request validation failed");
+
+        return next(err);
+      }
+    }
+
+    return callback;
+  }
+}
---
+++ test/middlewares/validation.middleware.test.ts
@@ -0,0 +1,118 @@
+import { expect } from "chai";
+import faker from "faker";
+import sinon from "sinon";
+import itParam from "mocha-param";
+import { ValidationMiddleware } from "../../src/middlewares/validation.middleware";
+import { ValidationService } from "../../src/services/validation.service";
+
+describe("ValidationMiddleware", () => {
+  let validationService: ValidationService;
+  let middleware: ValidationMiddleware;
+  let validationServicePrototypeMock: sinon.SinonMock;
+
+  before(() => {
+    validationService = new ValidationService();
+    middleware = new ValidationMiddleware(validationService);
+  });
+
+  beforeEach(() => {
+    validationServicePrototypeMock = sinon.mock(ValidationService.prototype);
+  });
+
+  afterEach(() => {
+    validationServicePrototypeMock.restore();
+  });
+
+  itParam(
+    "validates a request ${value}",
+    ["body", "query", "params"],
+    (value) => {
+      const req = {
+        [value]: {
+          age: faker.datatype.number().toString(),
+        },
+      };
+      const res = {
+        status(n: number) {
+          void n;
+
+          return this;
+        },
+        json(j: object) {
+          void j;
+
+          return this;
+        },
+      };
+      const next = () => undefined;
+
+      validationServicePrototypeMock
+        .expects("validate")
+        .once()
+        .withArgs(req[value])
+        .resolves(req[value]);
+
+      const result = middleware.validateRequest({
+        [`${value}Schema`]: {
+          type: "object",
+          properties: {
+            age: {
+              type: "string",
+            },
+          },
+          required: ["age"],
+        },
+      })(req, res, next);
+
+      return expect(result).to.eventually.be.undefined.and.to.satisfy(() => {
+        validationServicePrototypeMock.verify();
+
+        return true;
+      });
+    }
+  );
+
+  it("fails to validate a request", () => {
+    const req = {
+      body: {
+        age: faker.datatype.number().toString(),
+      },
+    };
+    const res = {
+      status(n: number) {
+        void n;
+
+        return this;
+      },
+      json(j: object) {
+        void j;
+
+        return this;
+      },
+    };
+    const next = () => undefined;
+
+    validationServicePrototypeMock
+      .expects("validate")
+      .once()
+      .withArgs(req.body)
+      .rejects("Validation failed");
+
+    const result = middleware.validateRequest({
+      bodySchema: {
+        type: "object",
+        properties: {
+          age: {
+            type: "string",
+          },
+        },
+      },
+    })(req, res, next);
+
+    return expect(result).to.eventually.be.undefined.and.to.satisfy(() => {
+      validationServicePrototypeMock.verify();
+
+      return true;
+    });
+  });
+});
