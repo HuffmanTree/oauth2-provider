09afd3fc012a13c032f5c4361876282ba99e1092
François Guérin <francoisguerin1@hotmail.fr>
2022-10-03 17:56:28 +0200

    Add error middleware

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,97 @@
+import { NextFunction, Request, Response } from "express";
+import { unknownToError } from "../utils";
+import { Logger } from "../logger";
+
+class HttpError extends Error {
+  constructor(readonly status: number, readonly name: string, original: Error) {
+    super(original.message);
+
+    this.stack = original.stack;
+  }
+}
+
+export class BadRequest extends HttpError {
+  constructor(original: Error) {
+    super(400, BadRequest.name, original);
+  }
+}
+
+export class Unauthorized extends HttpError {
+  constructor(original: Error) {
+    super(401, Unauthorized.name, original);
+  }
+}
+
+export class Forbidden extends HttpError {
+  constructor(original: Error) {
+    super(403, Forbidden.name, original);
+  }
+}
+
+export class NotFound extends HttpError {
+  constructor(original: Error) {
+    super(404, NotFound.name, original);
+  }
+}
+
+export class InternalServerError extends HttpError {
+  constructor(original: Error) {
+    super(500, InternalServerError.name, original);
+  }
+}
+
+export class ErrorMiddleware {
+  private _logger: Logger;
+
+  constructor() {
+    this._logger = new Logger({ service: "ErrorMiddleware" });
+  }
+
+  async notFound(
+    req: Request,
+    _res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    const original = new Error(`Path not found: '${req.path}'`);
+
+    next(new NotFound(original));
+  }
+
+  async handleError(
+    err: unknown,
+    _req: Request,
+    res: Response,
+    /**
+     * @todo 03-10-2022 - It is important to declare a `NextFunction`
+     * parameter so express treats this method as a
+     * `̀ErrorRequestHandler` and does not skip it. Forcing the type
+     * may help removing the eslint rule exception.
+     */
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _next: NextFunction
+  ): Promise<void> {
+    this._logger.error({ err }, "Raw error");
+
+    const httpError =
+      err instanceof HttpError
+        ? err
+        : new InternalServerError(unknownToError(err));
+
+    const json: {
+      message: string;
+      status: number;
+      name: string;
+      stack?: string;
+    } = {
+      message: httpError.message,
+      status: httpError.status,
+      name: httpError.name,
+    };
+
+    if (process.env.NODE_ENV === "development") json.stack = httpError.stack;
+
+    this._logger.error({ json }, "HTTP error");
+
+    res.status(json.status).json(json);
+  }
+}
---
+++ test/middlewares/error.middleware.test.ts
@@ -0,0 +1,185 @@
+import sinon from "sinon";
+import {
+  BadRequest,
+  Unauthorized,
+  Forbidden,
+  NotFound,
+  InternalServerError,
+  ErrorMiddleware,
+} from "../../src/middlewares/error.middleware";
+import faker from "faker";
+import itParam from "mocha-param";
+import { expect } from "chai";
+
+describe("Error classes", () => {
+  itParam<
+    [
+      (
+        | typeof BadRequest
+        | typeof Unauthorized
+        | typeof Forbidden
+        | typeof NotFound
+        | typeof InternalServerError
+      ),
+      string,
+      number
+    ]
+  >(
+    "Builds a ${value[0].name}",
+    [
+      [BadRequest, faker.datatype.string(), 400],
+      [Unauthorized, faker.datatype.string(), 401],
+      [Forbidden, faker.datatype.string(), 403],
+      [NotFound, faker.datatype.string(), 404],
+      [InternalServerError, faker.datatype.string(), 500],
+    ],
+    ([fn, message, expectedStatus]) => {
+      const err = new fn(new Error(message));
+
+      expect(err).to.be.instanceOf(fn);
+      expect(err)
+        .to.have.property("message")
+        .and.to.be.a("string")
+        .and.to.equal(message);
+      expect(err)
+        .to.have.property("name")
+        .and.to.be.a("string")
+        .and.to.equal(fn.name);
+      expect(err)
+        .to.have.property("status")
+        .and.to.be.a("number")
+        .and.to.equal(expectedStatus);
+    }
+  );
+});
+
+describe("ErrorMiddleware", () => {
+  let middleware: ErrorMiddleware;
+
+  before(() => {
+    middleware = new ErrorMiddleware();
+  });
+
+  it("falls to notFound - need mock objects for better test", () => {
+    const req = {};
+    const res = {
+      status(n: number) {
+        void n;
+
+        return this;
+      },
+      json(j: object) {
+        void j;
+
+        return this;
+      },
+    };
+    const next = () => undefined;
+
+    const result = middleware.notFound(req, res, next);
+
+    return expect(result).to.eventually.be.undefined;
+  });
+
+  it("falls to handleError with an 'HttpError' - need mock objects for better test", () => {
+    const message = "Missing property 'email'";
+    const err = new BadRequest(new Error(message));
+    const req = {};
+    const res = {
+      status(n: number) {
+        void n;
+
+        return this;
+      },
+      json(j: object) {
+        void j;
+
+        return this;
+      },
+    };
+    const next = () => undefined;
+    const statusSpy = sinon.spy(res, "status");
+    const jsonSpy = sinon.spy(res, "json");
+
+    const result = middleware.handleError(err, req, res, next);
+
+    expect(statusSpy.calledOnceWith(400)).to.be.true;
+    expect(
+      jsonSpy.calledOnceWith({
+        message: "Missing property 'email'",
+        status: 400,
+        name: "BadRequest",
+      })
+    ).to.be.true;
+
+    return expect(result).to.eventually.be.undefined;
+  });
+
+  it("falls to handleError with an 'Error' - need mock objects for better test", () => {
+    const message = "Uncaught error";
+    const err = new Error(message);
+    const req = {};
+    const res = {
+      status(n: number) {
+        void n;
+
+        return this;
+      },
+      json(j: object) {
+        void j;
+
+        return this;
+      },
+    };
+    const next = () => undefined;
+    const statusSpy = sinon.spy(res, "status");
+    const jsonSpy = sinon.spy(res, "json");
+
+    const result = middleware.handleError(err, req, res, next);
+
+    expect(statusSpy.calledOnceWith(500)).to.be.true;
+    expect(
+      jsonSpy.calledOnceWith({
+        message: "Uncaught error",
+        status: 500,
+        name: "InternalServerError",
+      })
+    ).to.be.true;
+
+    return expect(result).to.eventually.be.undefined;
+  });
+
+  it("falls to handleError with a string - need mock objects for better test", () => {
+    const message = "Uncaught string";
+    const err = message;
+    const req = {};
+    const res = {
+      status(n: number) {
+        void n;
+
+        return this;
+      },
+      json(j: object) {
+        void j;
+
+        return this;
+      },
+    };
+    const next = () => undefined;
+    const statusSpy = sinon.spy(res, "status");
+    const jsonSpy = sinon.spy(res, "json");
+
+    const result = middleware.handleError(err, req, res, next);
+
+    expect(statusSpy.calledOnceWith(500)).to.be.true;
+    expect(
+      jsonSpy.calledOnceWith({
+        message: "Uncaught string",
+        status: 500,
+        name: "InternalServerError",
+      })
+    ).to.be.true;
+
+    return expect(result).to.eventually.be.undefined;
+  });
+});
