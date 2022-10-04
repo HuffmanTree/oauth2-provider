122a89fd98fac8de003e92611a5c4608dc80c403
François Guérin <francoisguerin1@hotmail.fr>
2022-10-04 19:55:36 +0200

    Add user router

---
+++ src/routers/user.router.ts
@@ -0,0 +1,110 @@
+import { Router } from "express";
+import { ValidationMiddleware } from "src/middlewares/validation.middleware";
+import { UserController } from "../controllers/user.controller";
+import { JSONSchemaType } from "ajv";
+
+export class UserRouter {
+  readonly router: Router;
+
+  constructor(controller: UserController, middleware: ValidationMiddleware) {
+    this.router = Router();
+
+    {
+      const bodySchema: JSONSchemaType<{
+        email: string;
+        password: string;
+        name: string;
+      }> = {
+        type: "object",
+        properties: {
+          email: {
+            type: "string",
+            format: "email",
+            minLength: 1,
+          },
+          password: {
+            type: "string",
+            minLength: 1,
+          },
+          name: {
+            type: "string",
+            minLength: 1,
+          },
+        },
+        required: ["email", "password", "name"],
+      };
+
+      // CreateUser
+      this.router.post(
+        "/",
+        middleware
+          .validateRequest<
+            { email: string; password: string; name: string },
+            unknown,
+            unknown
+          >({ bodySchema })
+          .bind(middleware),
+        controller.create.bind(controller)
+      );
+    }
+
+    {
+      // GetUser
+      this.router.get(
+        "/:id(^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$)",
+        controller.find.bind(controller)
+      );
+    }
+
+    {
+      const bodySchema: JSONSchemaType<
+        Partial<{
+          email: string;
+          password: string;
+          name: string;
+        }>
+      > = {
+        type: "object",
+        properties: {
+          email: {
+            type: "string",
+            format: "email",
+            minLength: 1,
+            nullable: true,
+          },
+          password: {
+            type: "string",
+            minLength: 1,
+            nullable: true,
+          },
+          name: {
+            type: "string",
+            minLength: 1,
+            nullable: true,
+          },
+        },
+      };
+
+      // UpdateUser
+      this.router.patch(
+        "/:id(^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$)",
+        middleware
+          .validateRequest<
+            Partial<{ email: string; password: string; name: string }>,
+            unknown,
+            unknown
+          >({ bodySchema })
+          .bind(middleware),
+        controller.update.bind(controller)
+      );
+    }
+
+    {
+      // DestroyUser
+      this.router.delete(
+        "/:id(^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$)",
+        controller.destroy.bind(controller)
+      );
+    }
+  }
+}
---
+++ test/routers/user.router.test.ts
@@ -0,0 +1,51 @@
+import { expect } from "chai";
+import itParam from "mocha-param";
+import { UserController } from "../../src/controllers/user.controller";
+import { UserRouter } from "../../src/routers/user.router";
+import { ValidationMiddleware } from "../../src/middlewares/validation.middleware";
+
+describe("UserRouter", () => {
+  let router: UserRouter;
+
+  before(() => {
+    const controller = {
+      create: () => null,
+      find: () => null,
+      update: () => null,
+      destroy: () => null,
+    } as unknown as UserController;
+    const middleware = {
+      validateRequest: () => () => null,
+    } as unknown as ValidationMiddleware;
+    router = new UserRouter(controller, middleware);
+  });
+
+  itParam(
+    "ensures ${value[0]} ${value[1]} is present",
+    [
+      ["POST", "/"],
+      [
+        "GET",
+        "/:id(^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$)",
+      ],
+      [
+        "PATCH",
+        "/:id(^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$)",
+      ],
+      [
+        "DELETE",
+        "/:id(^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$)",
+      ],
+    ],
+    ([method, path]) => {
+      const route = router.router.stack.find((s) => {
+        if (s.route.path !== path) return false;
+        if (!s.route.methods[method.toLowerCase()]) return false;
+
+        return true;
+      });
+
+      expect(route).to.not.be.undefined;
+    }
+  );
+});
