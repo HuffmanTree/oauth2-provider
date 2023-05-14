7a1a02cfcc82c5561525c128ce879a7f6dc194fd
François Guérin <francoisguerin1@hotmail.fr>
2023-05-14 14:32:48 +0200

    Forbid projects to request authorization codes
    Projects are not allowed to request authorization code when:
    
    - not existing
    - requested redirect uri does not match with the one used at project
      registration
    - requested scope is not a subset of the one used at project registration

--- src/controllers/oauth2.controller.ts
+++ src/controllers/oauth2.controller.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response } from "express";
+import { ProjectService } from "../services/project.service";
 import { Logger } from "../logger";
 import { RequestService } from "../services/request.service";
+import { EmptyResultError } from "sequelize";
+import { Forbidden } from "../middlewares/error.middleware";
 
 type AuthorizeRequestQuery = {
   client_id: string,
@@ -12,12 +15,31 @@ type AuthorizeRequestQuery = {
 export class OAuth2Controller {
   private _logger: Logger;
 
-  private _service: RequestService;
+  private _projectService: ProjectService;
 
-  constructor(service: RequestService) {
-    this._service = service;
+  private _requestService: RequestService;
+
+  constructor(projectService: ProjectService, requestService: RequestService) {
+    this._projectService = projectService;
+
+    this._requestService = requestService;
 
     this._logger = new Logger({ service: "OAuth2Controller" });
+  }
+
+  /**
+   * @description Detects if error correspdonds to a forbidden scenario
+   *
+   * @param {unknown} err Error caught by `authorize` method
+   *
+   * @returns {boolean} Whether error should be translated to forbidden
+   */
+  private _isForbidden(err: unknown): boolean {
+    if (err instanceof EmptyResultError) return true;
+
+    if (err instanceof Error && err.message === "Disallowed request") return true;
+
+    return false;
   }
 
   async authorize(
@@ -26,24 +48,39 @@ export class OAuth2Controller {
     next: NextFunction
   ): Promise<void> {
     try {
-      const { client_id: clientId, redirect_uri, scope } = req.query;
+      const { client_id: clientId, redirect_uri } = req.query;
+      const scope = req.query.scope.split(",");
       const resourceOwner = res.locals.user;
+
+      const project = await this._projectService.findById(clientId);
+
+      if (!project.allowRequest(redirect_uri, scope)) {
+        this._logger.debug({ project: project.id }, "Disallowed request");
+
+        throw new Error("Disallowed request");
+      }
 
       this._logger.info(
         { resourceOwner, clientId, scope },
         "Create operation payload"
       );
 
-      const result = await this._service.create({
+      const result = await this._requestService.create({
         resourceOwner,
         clientId,
-        scope: scope.split(","),
+        scope,
       });
 
       const url = `${redirect_uri}?code=${result.code}`;
 
       res.redirect(url);
     } catch (err) {
+      if (this._isForbidden(err)) {
+        const original = new Error("Project not allowed to request");
+
+        return next(new Forbidden(original));
+      }
+
       next(err);
     }
   }
--- test/controllers/oauth2.controller.test.ts
+++ test/controllers/oauth2.controller.test.ts
@@ -6,28 +6,38 @@ import chai, { expect } from "chai";
 import faker from "faker";
 import chaiAsPromised from "chai-as-promised";
 import { RequestModel } from "../../src/models/request.model";
+import { ProjectService } from "../../src/services/project.service";
+import { ProjectModel } from "../../src/models/project.model";
+import { EmptyResultError } from "sequelize";
 
 chai.use(chaiAsPromised);
 
 describe("OAuth2Controller", () => {
-  const { request: model } = new OAuth2DatabaseClient({});
-  const service = new RequestService(model);
-  const controller = new OAuth2Controller(service);
+  const { request: model, project: projectModel } = new OAuth2DatabaseClient({});
+  const projectService = new ProjectService(projectModel);
+  const requestService = new RequestService(model);
+  const controller = new OAuth2Controller(projectService, requestService);
+  let projectServicePrototypeMock: sinon.SinonMock;
   let requestServicePrototypeMock: sinon.SinonMock;
 
   beforeEach(() => {
+    projectServicePrototypeMock = sinon.mock(ProjectService.prototype);
     requestServicePrototypeMock = sinon.mock(RequestService.prototype);
   });
 
   afterEach(() => {
+    projectServicePrototypeMock.restore();
     requestServicePrototypeMock.restore();
   });
 
   it("computes an oauth2 authorization code", () => {
+    const clientId = faker.datatype.uuid();
+    const redirectURI = faker.internet.url();
+    const scope = faker.datatype.array().map((i) => i.toString()).filter((i) => !i.includes(","));
     const payload = {
-      client_id: faker.datatype.uuid(),
-      redirect_uri: faker.internet.url(),
-      scope: faker.datatype.array().join(","),
+      client_id: clientId,
+      redirect_uri: redirectURI,
+      scope: scope.join(","),
     };
     const resourceOwner = faker.datatype.uuid();
     const req = { query: payload };
@@ -38,12 +48,18 @@ describe("OAuth2Controller", () => {
       locals: { user: resourceOwner },
     };
     const next = () => undefined;
+    const mockProject = new ProjectModel({ id: clientId, redirectURL: redirectURI, scope });
     const mockRequest = new RequestModel({
       clientId: payload.client_id,
       resourceOwner,
       scope: payload.scope.split(","),
     });
 
+    projectServicePrototypeMock
+      .expects("findById")
+      .once()
+      .withArgs(clientId)
+      .returns(mockProject);
     requestServicePrototypeMock
       .expects("create")
       .once()
@@ -63,7 +79,134 @@ describe("OAuth2Controller", () => {
       expect(redirect.calledOnceWith(`${payload.redirect_uri}?code=${mockRequest.code}`)).to.be.true;
       expect(nextSpy.notCalled).to.be.true;
 
+      projectServicePrototypeMock.verify();
       requestServicePrototypeMock.verify();
+
+      return true;
+    });
+  });
+
+  it("fails to compute an authorization code with an invalid client id", () => {
+    const clientId = faker.datatype.uuid();
+    const redirectURI = faker.internet.url();
+    const scope = faker.datatype.array().map((i) => i.toString()).filter((i) => !i.includes(","));
+    const payload = {
+      client_id: clientId,
+      redirect_uri: redirectURI,
+      scope: scope.join(","),
+    };
+    const resourceOwner = faker.datatype.uuid();
+    const req = { query: payload };
+    const res = {
+      redirect(url: string) {
+        void url;
+      },
+      locals: { user: resourceOwner },
+    };
+    const next = () => undefined;
+
+    projectServicePrototypeMock
+      .expects("findById")
+      .once()
+      .withArgs(clientId)
+      .throws(new EmptyResultError(""));
+    requestServicePrototypeMock.expects("create").never();
+
+    const redirect = sinon.spy(res, "redirect");
+
+    const result = controller.authorize(req, res, next);
+
+    return expect(result).to.eventually.be.undefined.and.to.satisfy(() => {
+      expect(redirect.notCalled).to.be.true;
+
+      projectServicePrototypeMock.verify();
+      requestServicePrototypeMock.verify();
+
+      redirect.restore();
+
+      return true;
+    });
+  });
+
+  it("fails to compute an authorization code with a disallowed request", () => {
+    const clientId = faker.datatype.uuid();
+    const redirectURI = faker.internet.url();
+    const scope = faker.datatype.array().map((i) => i.toString()).filter((i) => !i.includes(","));
+    const payload = {
+      client_id: clientId,
+      redirect_uri: redirectURI,
+      scope: scope.join(","),
+    };
+    const resourceOwner = faker.datatype.uuid();
+    const req = { query: payload };
+    const res = {
+      redirect(url: string) {
+        void url;
+      },
+      locals: { user: resourceOwner },
+    };
+    const next = () => undefined;
+
+    projectServicePrototypeMock
+      .expects("findById")
+      .once()
+      .withArgs(clientId)
+      .returns({ allowRequest: () => false });
+    requestServicePrototypeMock.expects("create").never();
+
+    const redirect = sinon.spy(res, "redirect");
+
+    const result = controller.authorize(req, res, next);
+
+    return expect(result).to.eventually.be.undefined.and.to.satisfy(() => {
+      expect(redirect.notCalled).to.be.true;
+
+      projectServicePrototypeMock.verify();
+      requestServicePrototypeMock.verify();
+
+      redirect.restore();
+
+      return true;
+    });
+  });
+
+  it("fails to compute an authorization code with another reason", () => {
+    const clientId = faker.datatype.uuid();
+    const redirectURI = faker.internet.url();
+    const scope = faker.datatype.array().map((i) => i.toString()).filter((i) => !i.includes(","));
+    const payload = {
+      client_id: clientId,
+      redirect_uri: redirectURI,
+      scope: scope.join(","),
+    };
+    const resourceOwner = faker.datatype.uuid();
+    const req = { query: payload };
+    const res = {
+      redirect(url: string) {
+        void url;
+      },
+      locals: { user: resourceOwner },
+    };
+    const next = () => undefined;
+
+    projectServicePrototypeMock
+      .expects("findById")
+      .once()
+      .withArgs(clientId)
+      .throws(new Error("Cannot access database"));
+    requestServicePrototypeMock.expects("create").never();
+
+    const redirect = sinon.spy(res, "redirect");
+
+    const result = controller.authorize(req, res, next);
+
+    return expect(result).to.eventually.be.undefined.and.to.satisfy(() => {
+      expect(redirect.notCalled).to.be.true;
+
+      projectServicePrototypeMock.verify();
+      requestServicePrototypeMock.verify();
+
+      redirect.restore();
 
       return true;
     });
