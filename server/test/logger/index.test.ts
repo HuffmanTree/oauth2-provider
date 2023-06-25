a52fcdb7d55663c03d9a927f4206a3dc7423566a
François Guérin <francoisguerin1@hotmail.fr>
2023-06-25 14:41:12 +0200

    Refactor logger test
    - Use `winstonMock` from helpers folder
    - Remove import from `itParam`, use native `forEach` instead
    - Remove import from `faker`, make values clear and readonly

--- server/test/logger/index.test.ts
+++ server/test/logger/index.test.ts
@@ -1,88 +1,28 @@
 import { expect } from "chai";
-import faker from "faker";
-import itParam from "mocha-param";
 import sinon from "sinon";
+import sinonTest from "sinon-test";
 import winston from "winston";
 import { Logger } from "../../src/logger";
+import { winstonMock } from "../helpers/mocks.helper";
+
+const test = sinonTest(sinon);
 
 describe("Logger", () => {
-  let mock: sinon.SinonMock;
   let logger: Logger;
-  const loggerMock = {
-    child(obj: Record<string, unknown>) {
-      void obj;
 
-      return this;
-    },
-    fatal(s: string) {
-      void s;
-
-      return this;
-    },
-    error(s: string) {
-      void s;
-
-      return this;
-    },
-    warn(s: string) {
-      void s;
-
-      return this;
-    },
-    info(s: string) {
-      void s;
-
-      return this;
-    },
-    debug(s: string) {
-      void s;
-
-      return this;
-    },
-    trace(s: string) {
-      void s;
-
-      return this;
-    },
-  };
-
-  before(() => {
-    mock = sinon.mock(winston);
-
-    mock.expects("createLogger").once().returns(loggerMock);
-
+  before(test(function() {
+    this.stub(winston, "createLogger").callsFake(() => winstonMock);
     logger = new Logger({ service: "Mocha" });
+  }));
 
-    mock.verify();
-  });
+  ["fatal", "error", "warn", "info", "debug", "trace"].forEach(level =>
+    it(`logs with a '${level}' level`, test(function() {
+      const child = this.spy(winstonMock, "child");
+      const fn = this.spy(winstonMock, level);
 
-  after(() => {
-    mock.restore();
-  });
+      logger[level]({ userId: "id" }, "My message");
 
-  itParam<{ level: "fatal" | "error" | "warn" | "info" | "debug" | "trace" }>(
-    "logs with a '${value.level}' level",
-    [
-      { level: "fatal" },
-      { level: "error" },
-      { level: "warn" },
-      { level: "debug" },
-      { level: "trace" },
-    ],
-    ({ level }) => {
-      const context = JSON.parse(faker.datatype.json());
-      const message = faker.lorem.sentence();
-
-      const childSpy = sinon.spy(loggerMock, "child");
-      const levelSpy = sinon.spy(loggerMock, level);
-
-      logger[level](context, message);
-
-      expect(childSpy.calledOnceWith({ context })).to.be.true;
-      expect(levelSpy.calledOnceWith(message)).to.be.true;
-
-      childSpy.restore();
-      levelSpy.restore();
-    },
-  );
+      expect(child.calledOnceWithExactly({ context: { userId: "id" } })).to.be.true;
+      expect(fn.calledOnceWithExactly("My message")).to.be.true;
+    })));
 });
