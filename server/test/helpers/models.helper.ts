d5f445f214395be73f63ebaef80c426affea0317
François Guérin <francoisguerin1@hotmail.fr>
2024-02-04 17:35:44 +0100

    Remove `sequelize-mock`
    This package seems to not be maintained anymore:
    
    - no types
    - differs from `sequelize`

--- server/test/helpers/models.helper.ts
+++ server/test/helpers/models.helper.ts
@@ -1,39 +1,38 @@
-import SequelizeMock from "sequelize-mock";
 import { ProjectModel } from "../../src/models/project.model";
 import { RequestModel } from "../../src/models/request.model";
 import { UserModel } from "../../src/models/user.model";
-
-function modelDefaultValues(attributes: Array<string>) {
-  const res = {};
-
-  for (const attribute of attributes)
-    res[attribute] = `default-${attribute.charAt(0).toUpperCase()}${attribute.slice(1)}`;
-
-  return res;
-}
 
 export function fakeUserModel({
   verifyPasswordReturnedValue,
 }: Partial<{
   verifyPasswordReturnedValue: boolean,
 }> = {}): typeof UserModel {
-  const sequelize = new SequelizeMock();
-  const model = sequelize.define("user", modelDefaultValues(["id", "email", "password", "givenName", "familyName", "picture", "phoneNumber", "birthdate"]), {
-    instanceMethods: {
+  function instance(p?: Partial<UserModel>) {
+    return {
+      ...p,
       verifyPassword() {
-        if (typeof verifyPasswordReturnedValue !== "boolean") throw new Error("Missing 'verifyPasswordReturnedValue' to mock 'verifyPassword' method");
-
-        return verifyPasswordReturnedValue;
+        return !!verifyPasswordReturnedValue;
       },
+      update(input: Partial<UserModel>) {
+        return { ...this, ...input };
+      },
+      destroy() { void 0; },
+      toJSON() { return this; },
+    };
+  }
+  const model = {
+    create(input: Partial<UserModel>) {
+      return instance(input);
     },
-  });
-
-  /**
-   * Method `findByPk` has not been implemented by `sequelize-mock` that still uses the old `findById`
-   * This line simply aliases `findByPk` to `findById`
-   * {@link https://github.com/BlinkUX/sequelize-mock/issues/71}
-   */
-  model.findByPk = model.findById.bind(model);
+    findByPk(id: string) {
+      return instance({
+        id,
+      });
+    },
+    findOne(options: { where: Partial<UserModel> } = { where: {} }) {
+      return instance(options.where);
+    },
+  } as unknown as typeof UserModel;
 
   return model;
 }
@@ -45,45 +44,64 @@ export function fakeProjectModel({
   allowRequestReturnedValue: boolean,
   verifySecretReturnedValue: boolean,
 }> = {}): typeof ProjectModel {
-  const sequelize = new SequelizeMock();
-  const model = sequelize.define("project", modelDefaultValues(["id", "secret", "name", "redirectURL", "scope", "creator"]), {
-    instanceMethods: {
+  function instance(p?: Partial<ProjectModel>) {
+    return {
+      ...p,
       allowRequest() {
-        if (typeof allowRequestReturnedValue !== "boolean") throw new Error("Missing 'allowRequestReturnedValue' to mock 'allowRequest' method");
-
-        return allowRequestReturnedValue;
+        return !!allowRequestReturnedValue;
       },
       verifySecret() {
-        if (typeof verifySecretReturnedValue !== "boolean") throw new Error("Missing 'verifySecretReturnedValue' to mock 'verifySecret' method");
-
-        return verifySecretReturnedValue;
+        return !!verifySecretReturnedValue;
       },
-      setDataValue(key: string, value: unknown) {
-        this[key] = value;
+      update(input: Partial<ProjectModel>) {
+        return { ...this, ...input };
       },
+      destroy() { void 0; },
+      toJSON() { return this; },
+      setDataValue() { void 0; },
+    };
+  }
+  const model = {
+    create(input: Partial<ProjectModel>) {
+      return instance(input);
     },
-  });
-
-  /**
-   * Method `findByPk` has not been implemented by `sequelize-mock` that still uses the old `findById`
-   * This line simply aliases `findByPk` to `findById`
-   * {@link https://github.com/BlinkUX/sequelize-mock/issues/71}
-   */
-  model.findByPk = model.findById.bind(model);
+    findByPk(id: string) {
+      return instance({
+        id,
+      });
+    },
+    findOne(options: { where: Partial<ProjectModel> } = { where: {} }) {
+      return instance(options.where);
+    },
+  } as unknown as typeof ProjectModel;
 
   return model;
 }
 
 export function fakeRequestModel(): typeof RequestModel {
-  const sequelize = new SequelizeMock();
-  const model = sequelize.define("request", modelDefaultValues(["id", "resourceOwner", "clientId", "scope", "code", "token"]));
-
-  /**
-   * Method `findByPk` has not been implemented by `sequelize-mock` that still uses the old `findById`
-   * This line simply aliases `findByPk` to `findById`
-   * {@link https://github.com/BlinkUX/sequelize-mock/issues/71}
-   */
-  model.findByPk = model.findById.bind(model);
+  function instance(p?: Partial<RequestModel>) {
+    return {
+      ...p,
+      update(input: Partial<RequestModel>) {
+        return { ...this, ...input };
+      },
+      destroy() { void 0; },
+      toJSON() { return this; },
+    };
+  }
+  const model = {
+    create(input: Partial<RequestModel>) {
+      return instance(input);
+    },
+    findByPk(id: string) {
+      return instance({
+        id,
+      });
+    },
+    findOne(options: { where: Partial<RequestModel> } = { where: {} }) {
+      return instance(options.where);
+    },
+  } as unknown as typeof RequestModel;
 
   return model;
 }
--- server/test/types/sequelize-mock.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "sequelize-mock" {
-  import { Sequelize } from "sequelize";
-
-  class SequelizeMock extends Sequelize {
-    public abstract define(modelName: string, values: object, options?: object);
-  }
-
-  export default SequelizeMock;
-}
